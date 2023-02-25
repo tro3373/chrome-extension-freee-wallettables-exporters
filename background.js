ebc176035f39f124b98cc9d736dea9b91f49756a
tro3373 <tro3373@gmail.com>
2023-02-25 21:39:53 +0900

    feat: Upgrade to v3

--- background.js
+++ background.js
@@ -1,16 +1,10 @@
-// "content_scripts": [
-//   {
-//     "matches": [
-//       "https://secure.freee.co.jp/wallet/walletables/"
-//     ],
-//     "js": [
-//       "jquery-3.6.0.min.js",
-//       "main.js"
-//     ]
-//   }
-// ]
 /* eslint no-undef: 0 */
-chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(tab.id, { file: 'jquery-3.6.0.min.js' });
-  chrome.tabs.executeScript(tab.id, { file: 'content.js' });
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+      target: {tabId: tab.id, allFrames: false},
+      files: [
+        'jquery-3.6.0.min.js',
+        'content.js'
+      ],
+  });
 });
